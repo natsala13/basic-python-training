293b31f670acf0e2cfb8c95d97a9a79d5b00c8ba
AsafG1 <asaf.green1@gmail.com>
2020-03-23 18:45:49 +0200

    CHAP_3_4

---
+++ Chap_5/doubling_func.py
@@ -0,0 +1,24 @@
+#Page 64
+def doubling(a,b):
+    # Fucntion double a * b and returns
+    return a*b
+print(doubling(5,6))
+
+#Page 65
+def division(a,b):
+    if a*b == 0 :
+        return 'Illegal'
+    return a/b
+
+print(division(0,6))
+
+#Page 71
+
+def factorial():
+    return 1*2*3*4*5
+def beep(string):
+    return string +' beep'
+def mul_2nums(a,b):
+    if a*b<0
+        return 0
+    return a*b
