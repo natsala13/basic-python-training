3ad863004e3a3e65e6f1b00599b6ac921d371045
shani afek <shaniafek@gmail.com>
2020-03-26 00:38:39 +0200

    Abanibi.py

---
+++ Abanibi.py
@@ -0,0 +1,16 @@
+message = input('please enter your sentence\n')
+out = str()
+for i in range(0, len(message)):
+    if message[i] == 'a':
+        out += message[i] + "b" + message[i]
+    elif message[i] == 'e':
+        out += message[i] + "b" + message[i]
+    elif message[i] == 'u':
+        out += message[i] + "b" + message[i]
+    elif message[i] == 'o':
+        out += message[i] + "b" + message[i]
+    elif message[i] == 'i':
+        out += message[i] + "b" + message[i]
+    else:
+        out += message[i]
+print(out)
