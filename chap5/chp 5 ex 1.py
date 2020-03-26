0c3fbdb5d3430245e776b0b02c0646aeccb45832
Itailasri <62516697+Itailasri@users.noreply.github.com>
2020-03-26 19:18:55 +0200

    Add files via upload

---
+++ chap5/chp 5 ex 1.py
@@ -0,0 +1,4 @@
+def times(val1, val2):
+    return val1 * val2
+
+print(times(6, 3))
---
+++ chap5/division.py
@@ -0,0 +1,15 @@
+def devide(val1, val2):
+    if val2 == 0:
+        return 'illegal'
+    else:
+        return val1 // val2
+print(devide(6,3))
+
+
+
+
+
+
+
+
+
