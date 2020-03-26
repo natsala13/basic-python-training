4049727418eed9052d5b818ef0bcffd6def83eab
Itailasri <62516697+Itailasri@users.noreply.github.com>
2020-03-26 21:11:08 +0200

    Add files via upload

---
+++ cahpter3/fibonnaci.py
@@ -0,0 +1,21 @@
+a = -1
+b = 1
+while True:
+    a = a + b
+    b = b + a
+    print(a)
+    print(b)
+    if b + a > 10000:
+        break
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ cahpter3/range.py
@@ -0,0 +1,2 @@
+for num in range(1, 41, 1):
+    print(num)
